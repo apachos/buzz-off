30397a7f7d3ad976a34e7abc85d977d6faf0022b
Scott Laska <actsone8@gmail.com>
2022-05-09 11:10:58 -0500

    Added router outlet for home page

---
+++ public/scripts/home.js
@@ -0,0 +1,11 @@
+function displayHome() {
+  let pageOutlet = document.getElementById('page-outlet');
+
+  pageOutlet.innerHTML = `
+    <div class="header">
+      <h1 class="header__title">India</h1>
+      <p class="header__caption">Delhi India Gate</p>
+      <a class="header__button" href="#" onclick="releaseBugs()">Explore</a>
+    </div>
+  `;
+}
