7fc5f706a22480c43ca970063e802b6223c73b7b
Scott Laska <actsone8@gmail.com>
2022-05-11 09:34:30 -0500

    Made unspeakable function in explore.js

--- public/scripts/explore.js
+++ public/scripts/explore.js
@@ -1,10 +1,19 @@
 let slideIndex = 1;
 
-function explore() {
-  fetch('http://localhost:3000/api/attractions')
-    .then(response => response.json())
-    .then(data => displayAttractions(data))
-    .catch(error => console.error('Error:', error));
+function explore() {fetch('http://localhost:3000/api/attractions')
+.then(response => response.json()).then(data => {
+let pageOutlet = document.getElementById('page-outlet');
+pageOutlet.innerHTML = `<div class="attractions">
+<div class="carousel">
+${attractions.forEach(attraction => {slideHtml += `
+<div class="carousel__slide">
+<h3><a href="${attraction.site}">${attraction.name}</a></h3>
+<address>${attraction.address}</address>
+<p>(ranked #${attraction.rank})</p>
+<q>${attraction.description}</q>
+</div>`;})}<a class="carousel__prev" onclick="plusSlides(-1)">❮</a>
+<a class="carousel__next" onclick="plusSlides(1)">❯</a></div></div>`;
+showSlides(slideIndex);)}).catch(error => console.error('Error:', error));
 }
 
 function displayAttractions(attractions) {
